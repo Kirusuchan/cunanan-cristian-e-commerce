2707569ae6875c542fa6bce486995e3a249fe6b4
Cristian Cunanan <cunanancristian5@gmail.com>
2025-04-02 10:33:59 +0800

    success!

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -1,7 +1,8 @@
-<?php
+<?php 
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [HomeController::class, 'index']); 
+?>
